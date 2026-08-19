96ecb0749254c3f617b67ac8bf63fe0484cccbe3
Dela Anthonio <dela@anthonio.pro>
2026-08-19 17:02:41 -0400

    no-mistakes(lint): Suppress sourced effort label ShellCheck false positive

--- bin/fm-profile-lib.sh
+++ bin/fm-profile-lib.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2034 # Sourced callers use this diagnostic label.
 FM_PROFILE_EFFORT_VALUES="low, medium, high, xhigh, max, dynamic"
 
 fm_profile_effort_valid() {
